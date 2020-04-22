e86b16683bc3d76ec9035d2535329d223f9f6b01
Alex Shuryhin <oshuryhin@intellias.com>
2020-04-22 15:54:27 +0300

    Create users app

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'opencourse.courses.apps.CoursesConfig',
+    'opencourse.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
