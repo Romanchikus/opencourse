a5e7ee43308437940ccb6e03f6c9f42e8eec66a0
Alex Shuryhin <oshuryhin@intellias.com>
2020-04-22 15:12:15 +0300

    Restructure like cookicutter (settings -> config)

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'opencourse.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'opencourse.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
