3c1f5c19d7372d8bcb2b10d868f9833396c84cfb
Alex Shuryhin <oshuryhin@intellias.com>
2020-04-22 15:19:27 +0300

    Restructure like cookicutter (add opencourse folder for applications: courses -> opencourse/courses)

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CoursesConfig',
+    'opencourse.courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'opencourse/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
