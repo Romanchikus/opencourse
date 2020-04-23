f9c88d2c82e5d94f4a79b570e24a0ef76f9201d3
Alex Shuryhin <oshuryhin@intellias.com>
2020-04-23 12:03:18 +0300

    Add login/logout basic flows

--- opencourse/users/urls.py
+++ opencourse/users/urls.py
@@ -4,4 +4,6 @@ from opencourse.users import views
 app_name = "users"
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
--- opencourse/users/views.py
+++ opencourse/users/views.py
@@ -1,9 +1,26 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from opencourse.users.forms import UserCreationForm
+from django.contrib.auth.views import (
+    LoginView as BaseLoginView,
+    LogoutView as BaseLogoutView,
+)
 
 
 class SignUpView(CreateView):
     template_name = "users/signup.html"
     form_class = UserCreationForm
     success_url = reverse_lazy("users:signup")
+
+
+class LoginView(BaseLoginView):
+    template_name = "users/login.html"
+
+    def get_success_url(self):
+        redirect_url = self.get_redirect_url()
+        return redirect_url or reverse_lazy("users:signup")
+
+
+class LogoutView(BaseLogoutView):
+    def get_next_page(self):
+        return reverse_lazy("users:signup")
