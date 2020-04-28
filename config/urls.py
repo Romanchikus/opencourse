7b7b48dd1222b7e21dc89d0a27c7952ceed7b2aa
Alex Shuryhin <oshuryhin@intellias.com>
2020-04-28 23:00:26 +0300

    Install django-allauth

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('apps.courses.urls', namespace="courses")),
     path('users/', include('apps.users.urls', namespace="users")),
+    path('accounts/', include('allauth.urls')),
 ]
