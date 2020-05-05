33bb0b34e195a42cf545840431f3d1d647239a4c
Alex Shuryhin <oshuryhin@intellias.com>
2020-05-05 14:49:28 +0300

    Ad contact_request functinoality

--- opencourse/static/js/professor_detail.js
+++ opencourse/static/js/professor_detail.js
@@ -1,15 +1,39 @@
 $(function () {
-    options = {
-        min:0,
-        max:5,
-        step:1,
-        size:'sm',
-        showClear:false,
-        showCaption:false,
-        theme:'krajee-uni',
-    }
-    $("#id_score").rating(options);
+  options = {
+    min: 0,
+    max: 5,
+    step: 1,
+    size: 'sm',
+    showClear: false,
+    showCaption: false,
+    theme: 'krajee-uni',
+  }
+  $("#id_score").rating(options);
 
-    options.displayOnly = true
-    $(".score-display").rating(options);
+  options.displayOnly = true
+  $(".score-display").rating(options);
+
+  var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
 });
+
+function contact_request() {
+  var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
+
+  var url = $("#contact_request").attr("data-ajax-target")
+  $.post(url)
+  console.log(url)
+};
