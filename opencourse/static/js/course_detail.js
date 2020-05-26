bff38c4990e7d41362530235eeac92f15ee3f1c6
Alex Shuryhin <oshuryhin@intellias.com>
2020-05-27 00:42:02 +0300

    Edit list_enrollments.html

--- opencourse/static/js/course_detail.js
+++ opencourse/static/js/course_detail.js
@@ -1,14 +1,14 @@
 function contact_request() {
-    var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-        if (!this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-        }
-    });
+  var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
 
-    var url = $("#contact_request").attr("data-ajax-target")
-    $.post(url)
-    console.log(url)
+  var url = $("#contact_request").attr("data-ajax-target")
+  $.post(url)
+  console.log(url)
 };
